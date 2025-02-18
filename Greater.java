2bee22a16feb231eec4599c8908b7081421a2ab4
Shweta Shetty <shettyshweta48@gmail.com>
2025-02-18 10:20:46 +0530

    Add files via upload

---
+++ Greater.java
@@ -0,0 +1,12 @@
+//wap to find greatest of 2 numbers using conditonal operator:
+public class Greater {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        if (a > b) {
+            System.out.println("The greater number is: " + a);
+        } else {
+            System.out.println("The greater number is: " + b);
+        }
+    }
+}
