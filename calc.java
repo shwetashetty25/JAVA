1974d0ba541f5d517036f2938c4660f6caec83e9
Shweta Shetty <shettyshweta48@gmail.com>
2025-03-24 13:14:56 +0530

    Add files via upload

---
+++ calc.java
@@ -0,0 +1,6 @@
+// wap in which you have to enter two numbers, you have to display a menu for
+// users, in which you have to ask a operation of add, sub,mul,div,modulus,exit.
+// (use loop and switch case)
+class calc{
+    
+}
